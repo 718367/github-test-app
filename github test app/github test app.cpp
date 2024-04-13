dd963f59d693bfa0f022cf850b9451a8ca327695
yousef <yousef@gmail.com>
2024-04-13 17:23:48 +0200

    Add project files

--- github test app/github test app.cpp
+++ github test app/github test app.cpp
@@ -1,31 +1,34 @@
-// github test app.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+using namespace std;
+
+void expand(int arr[], int size);
+
 
 int main()
 {
-    std::cout << "this is yousef\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    for (int i = 0; i < 10; i++)
-        std::cout << i << std::endl;
+    int size = 3;
+    int* arr = new int[size];
+    bool  Continue =1;
+    while(Continue){
+        int i = 0;
+        cin >> arr[i];
+        i++;
+        if (i == size) {
+            cout << "wait for expanding";
+            expand(arr, size);
+        }
+        cout << "To Continue press 1 \nTo Exit press 0" << endl;
+        cin >> Continue;
+    }
+        
+   
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void expand(int arr[], int size) {
+    int* temp = new int[size+1];
+    for (int i = 0; i < size; i++)
+        temp[i] = arr[i];
+    arr = temp;
+    delete[] temp;
+}
